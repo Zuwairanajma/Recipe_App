6878bedb07521adba98508b47879d8a769dbaf57
Mike Dark <agyemankojo47@gmail.com>
2023-11-20 23:43:48 +0000

    Create data model data class

---
+++ app/models/recipe_food.rb
@@ -0,0 +1,7 @@
+class RecipeFood < ApplicationRecord
+  belongs_to :recipe
+  belongs_to :food
+  belongs_to :user
+
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+end
