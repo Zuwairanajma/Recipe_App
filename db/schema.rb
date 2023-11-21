9c3ac2a8071d9ebedf617339a1e43279314a9647
Mike Dark <agyemankojo47@gmail.com>
2023-11-21 17:08:15 +0000

    Soft rubocop fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,48 +10,48 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_20_225454) do
-  create_table "foods", force: :cascade do |t|
-    t.string "name"
-    t.string "measurement_unit"
-    t.decimal "price"
-    t.integer "quantity"
-    t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_foods_on_user_id"
+ActiveRecord::Schema[7.1].define(version: 20_231_120_225_454) do
+  create_table 'foods', force: :cascade do |t|
+    t.string 'name'
+    t.string 'measurement_unit'
+    t.decimal 'price'
+    t.integer 'quantity'
+    t.integer 'user_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_foods_on_user_id'
   end
 
-  create_table "recipe_foods", force: :cascade do |t|
-    t.integer "recipe_id", null: false
-    t.integer "food_id", null: false
-    t.integer "quantity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["food_id"], name: "index_recipe_foods_on_food_id"
-    t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
+  create_table 'recipe_foods', force: :cascade do |t|
+    t.integer 'recipe_id', null: false
+    t.integer 'food_id', null: false
+    t.integer 'quantity'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['food_id'], name: 'index_recipe_foods_on_food_id'
+    t.index ['recipe_id'], name: 'index_recipe_foods_on_recipe_id'
   end
 
-  create_table "recipes", force: :cascade do |t|
-    t.string "name"
-    t.integer "preparation_time"
-    t.integer "cooking_time"
-    t.text "description"
-    t.boolean "public"
-    t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_recipes_on_user_id"
+  create_table 'recipes', force: :cascade do |t|
+    t.string 'name'
+    t.integer 'preparation_time'
+    t.integer 'cooking_time'
+    t.text 'description'
+    t.boolean 'public'
+    t.integer 'user_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_recipes_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  add_foreign_key "foods", "users"
-  add_foreign_key "recipe_foods", "foods"
-  add_foreign_key "recipe_foods", "recipes"
-  add_foreign_key "recipes", "users"
+  add_foreign_key 'foods', 'users'
+  add_foreign_key 'recipe_foods', 'foods'
+  add_foreign_key 'recipe_foods', 'recipes'
+  add_foreign_key 'recipes', 'users'
 end
